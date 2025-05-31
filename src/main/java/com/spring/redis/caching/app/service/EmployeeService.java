770b67f108db174193bb1a48ab18072039e11e0e
HrutikS <hrutiksurwade.work@gmail.com>
2025-05-31 23:06:48 +0530

    Code changes: + Simplified cache flow + Removed CacheUtil

--- src/main/java/com/spring/redis/caching/app/service/EmployeeService.java
+++ src/main/java/com/spring/redis/caching/app/service/EmployeeService.java
@@ -2,8 +2,8 @@ package com.spring.redis.caching.app.service;
 
 import com.spring.redis.caching.app.domains.Employee;
 import com.spring.redis.caching.app.repository.EmployeeRepository;
-import com.spring.redis.caching.app.util.CacheUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -17,27 +17,17 @@ public class EmployeeService {
     @Autowired
     private CacheManager cacheManager;
 
-    @Autowired
-    private CacheUtil cacheUtil;
-
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    @Cacheable(value = "employee", key = "#employee.empId")
     public Employee registerEmployeeService(Employee employee) {
         return employeeRepository.save(employee);
     }
 
+    @Cacheable(value = "employee", key = "#empId")
     public Employee getEmployeeService(String empId) {
 
-        // Check cache and get employee
-        Employee cachedEmployee = cacheUtil.getCachedEmployee(empId);
+        Cache cache = cacheManager.getCache("employee");
 
-        // If cached employee exists, return it
-        if (cachedEmployee != null) {
-            return cachedEmployee;
-        }
-
-        // Fetch from repository
         return employeeRepository.findById(empId).orElse(null);
     }
 }
